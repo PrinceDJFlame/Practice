8c5938395c7e51585a71f9f76b83f24e146472ea
PrinceDJFlame <121134847+PrinceDJFlame@users.noreply.github.com>
2024-06-11 18:47:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+    music = loadSound("music1.mp3");
+}
+
+function play(){
+    music.play();
+}
+
+function setup(){
+    if pause_music() = true{
+        music.play() = false
+    }
+}
